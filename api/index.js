3d03843e81f2808dafe3bf285692a7e4bf2a0e72
Divya Tank <satyamtank03@gmail.com>
2023-08-01 10:36:53 +0530

    Rest Api

---
+++ api/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+// connect database...
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const authRoute = require("./routes/auth");
+// const userRoute = require("./routes/users");
+// const postRoute = require("./routes/posts");
+
+// const app = express();
+// const PORT = 5000;
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// // connect database...
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(console.log("Connected to MongoDB"))
+//   .catch((err) => console.log(err));
+
+// app.use(express.json());
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+
+// app.listen(PORT, (error) => {
+//   if (!error) {
+//     console.log(`Server is Successfully Running on Port ${PORT}`);
+//   } else {
+//     console.log("Error occurred, server can't start", error);
+//   }
+// });
